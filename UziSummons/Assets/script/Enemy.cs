e36c5fade55acfc7de971f0bc5be7f9ce777fb99
Tuukka Kurtti <tuukkak123@gmail.com>
2016-01-29 23:38:26 +0200

    added some enemy code

---
+++ UziSummons/Assets/script/Enemy.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+    public float hp;
+    bool isAlive = true;
+    public Vector2 targetPosition;
+
+    // Use this for initialization
+    void Start () {
+        if (hp == 0) hp = 50F;
+
+	}
+
+    void FixedUpdate() {
+
+        if (isAlive)
+        {
+            Rigidbody2D rbody = GetComponent<Rigidbody2D>();
+
+            if (true) { // TODO (mode == flying) {
+                Vector2 forcePosition = (targetPosition - (Vector2)transform.position);
+                Vector2 forceDirection = forcePosition.normalized;
+                float speed = forcePosition.magnitude;
+                Vector2 force = forceDirection.normalized * speed;
+
+                rbody.AddForce(force);
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	    if (hp <= 0)
+        {
+            isAlive = false;
+        }
+
+        if (isAlive)
+        {
+            UpdateAi();
+        }
+	}
+
+    protected virtual void UpdateAi()
+    {
+
+    }
+
+    protected virtual void Shoot(GameObject target)
+    {
+
+    }
+}
+
+
---
+++ UziSummons/Assets/script/FlyingDemon.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyingDemon : Enemy
+{
+    
+    public enum AIstate { AI_SUMMONED, AI_SHOOTING, AI_SWAP_POS, AI_FLYING };
+    public AIstate state;
+
+    protected override void UpdateAi()
+    {
+        //base.UpdateAi();
+
+        // Spawn position (-10, 10) - (10, 10) 
+
+        if (state == AIstate.AI_SUMMONED)
+        {
+            state = AIstate.AI_SWAP_POS;
+            targetPosition = new Vector2(Random.Range(-9, 9), Random.Range(5, 8)); 
+        }
+
+
+
+    }
+
+}
