c17166832ff4895c3ab3bd3d87045e8dcfe3fa52
Jukka Länsineva <jukka.lansineva@student.oulu.fi>
2016-01-30 12:18:39 +0200

    fix

--- UziSummons/Assets/Scripts/SpawnBehaviour.cs
+++ UziSummons/Assets/Scripts/SpawnBehaviour.cs
@@ -9,8 +9,12 @@ public class SpawnBehaviour : MonoBehaviour {
     Level currentLevel;
     [SerializeField]
     int currentWave = 0;
+    int maxWaves = 0;
     bool waveSpawnedCompletely = false;
+    bool enemiesAreDead = false;
     public GameObject player;
+    //public GameObject LevelListContainer;
+
 	// Use this for initialization
 	void Start () {
         LoadLevelData("level1.txt");
@@ -35,6 +39,10 @@ public class SpawnBehaviour : MonoBehaviour {
                         GameObject spawnedEnemy;
                         spawnedEnemy = (GameObject)Instantiate(Resources.Load("Prefabs/" + currentLevel.enemies[currentWave].enemyPrefab), Vector3.zero, Quaternion.identity);  // prefabs need to go to Resources/Prefabs or otherwise everything is terrible
                         spawnedEnemy.GetComponent<Enemy>().SetData(currentLevel.enemies[currentWave]);
+                        if (this.player != null)
+                        {
+                            spawnedEnemy.GetComponent<Enemy>().player = this.player;
+                        }
                     }
 
                     currentLevel.enemies[currentWave].amount -= 1;
@@ -49,14 +57,27 @@ public class SpawnBehaviour : MonoBehaviour {
 	}
 
     void SpawnWave() {
-        currentWave += 1;
-        currentNewWaveTimer = 0.0f;
+        // if we at maxwaves, load next level when everyone's dead
+        if (currentWave < maxWaves) {
+            currentWave += 1;
+            currentNewWaveTimer = 0.0f;
+        } else
+        {
+            if(enemiesAreDead)
+            {
+                LoadLevelData("level1.txt");
+            }
+        }
+
+        
     }
 
     public void LoadLevelData(string levelName) {
         string path = "LevelData/" + levelName.Replace(".txt", "");  
         string jsonData = Resources.Load<TextAsset>(path).text;
         currentLevel = JsonUtility.FromJson<Level>(jsonData);
+        maxWaves = currentLevel.enemies.Length;
         currentNewWaveTimer = 0.0f;
+        currentWave = 0;
     }
 }
