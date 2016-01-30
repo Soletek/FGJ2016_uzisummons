33085d84d4750506a418ab6a8725cc7cc49a34df
Jukka Länsineva <jukka.lansineva@student.oulu.fi>
2016-01-30 02:58:17 +0200

    branch initial

---
+++ UziSummons/Assets/Scripts/LevelEnemyDataCollection.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+[Serializable]
+public class LevelEnemyDataCollection {
+    public string enemyPrefab;
+    public float amount;
+    public int pattern;
+}
