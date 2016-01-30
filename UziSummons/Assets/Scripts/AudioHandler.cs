50c92541a7371e5ff36bd5577fb8528be708ff93
Tuukka Kurtti <tuukkak123@gmail.com>
2016-01-30 17:54:23 +0200

    added sounds

---
+++ UziSummons/Assets/Scripts/AudioHandler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void PlaySound(string name)
+    {
+        GameObject sound = (GameObject)Instantiate(Resources.Load("Audio/" + name));
+        sound.transform.SetParent(this.transform);
+    }
+}
